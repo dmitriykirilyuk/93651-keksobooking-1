e1fba60f4843e8961d178add350bc3c862f4a791
dmitriykirilyuk <dimasishere@gmail.com>
2017-04-20 01:03:55 +0300

    доработал модули

--- js/card.js
+++ js/card.js
@@ -1,42 +1,44 @@
 'use strict';
-window.createDialog = (function (newDialog) {
+(function () {
   var template = document.querySelector('#lodge-template');
-  var templateCopy = template.content.cloneNode(true);
-  var templateData = newDialog;
-  var lodgeTitle = templateCopy.querySelector('.lodge__title');
-  var lodgeAddress = templateCopy.querySelector('.lodge__address');
-  var lodgePrice = templateCopy.querySelector('.lodge__price');
-  var lodgeType = templateCopy.querySelector('.lodge__type');
-  var lodgeRooms = templateCopy.querySelector('.lodge__rooms-and-guests');
-  var lodgeFeatures = templateCopy.querySelector('.lodge__features');
-  var lodgeCheckIn = templateCopy.querySelector('.lodge__checkin-time');
-  var lodgeDescription = templateCopy.querySelector('.lodge__description');
-  var offerDialog = document.querySelector('#offer-dialog');
-  var lodgePanel = offerDialog.querySelector('.dialog__panel');
-  var dialogTitle = offerDialog.querySelector('.dialog__title');
-  var dialogAvatar = dialogTitle.querySelector('img');
-  var offerList = templateData.offer;
-  lodgeTitle.textContent = offerList.title;
-  lodgeAddress.textContent = offerList.address;
-  lodgePrice.textContent = offerList.price + '&#x20bd;/ночь';
-  if (offerList.type === 'flat') {
-    lodgeType.textContent = 'Квартира';
-  } else if (offerList.type === 'bungalo') {
-    lodgeType.textContent = 'Бунгало';
-  } else {
-    lodgeType.textContent = 'Дом';
-  }
-  lodgeRooms.textContent = 'Для ' + offerList.price + ' гостей в ' + offerList.rooms + ' комнатах';
-  lodgeCheckIn.textContent = 'Заезд после ' + offerList.checkin + ', выезд до ' + offerList.checkout;
-  var featuresFragment = document.createDocumentFragment();
-  for (var k = 0; k < offerList.features.length; k++) {
-    var featuresList = offerList.features[k];
-    var featuresSpan = document.createElement('span');
-    featuresSpan.classList.add('feature__image', 'feature__image--' + featuresList);
-    // featuresFragment.appendChild(featuresSpan);
-  }
-  lodgeFeatures.appendChild(featuresFragment);
-  lodgeDescription.textContent = offerList.description;
-  dialogAvatar.src = templateData.author.avatar;
-  offerDialog.replaceChild(templateCopy, lodgePanel);
+  window.createDialog = function (newDialog) {
+    var templateCopy = template.content.cloneNode(true);
+    var templateData = newDialog;
+    var lodgeTitle = templateCopy.querySelector('.lodge__title');
+    var lodgeAddress = templateCopy.querySelector('.lodge__address');
+    var lodgePrice = templateCopy.querySelector('.lodge__price');
+    var lodgeType = templateCopy.querySelector('.lodge__type');
+    var lodgeRooms = templateCopy.querySelector('.lodge__rooms-and-guests');
+    var lodgeFeatures = templateCopy.querySelector('.lodge__features');
+    var lodgeCheckIn = templateCopy.querySelector('.lodge__checkin-time');
+    var lodgeDescription = templateCopy.querySelector('.lodge__description');
+    var offerDialog = document.querySelector('#offer-dialog');
+    var lodgePanel = offerDialog.querySelector('.dialog__panel');
+    var dialogTitle = offerDialog.querySelector('.dialog__title');
+    var dialogAvatar = dialogTitle.querySelector('img');
+    var offerList = templateData.offer;
+    lodgeTitle.textContent = offerList.title;
+    lodgeAddress.textContent = offerList.address;
+    lodgePrice.textContent = offerList.price + '&#x20bd;/ночь';
+    if (offerList.type === 'flat') {
+      lodgeType.textContent = 'Квартира';
+    } else if (offerList.type === 'bungalo') {
+      lodgeType.textContent = 'Бунгало';
+    } else {
+      lodgeType.textContent = 'Дом';
+    }
+    lodgeRooms.textContent = 'Для ' + offerList.price + ' гостей в ' + offerList.rooms + ' комнатах';
+    lodgeCheckIn.textContent = 'Заезд после ' + offerList.checkin + ', выезд до ' + offerList.checkout;
+    var featuresFragment = document.createDocumentFragment();
+    for (var k = 0; k < offerList.features.length; k++) {
+      var featuresList = offerList.features[k];
+      var featuresSpan = document.createElement('span');
+      featuresSpan.classList.add('feature__image', 'feature__image--' + featuresList);
+      // featuresFragment.appendChild(featuresSpan);
+    }
+    lodgeFeatures.appendChild(featuresFragment);
+    lodgeDescription.textContent = offerList.description;
+    dialogAvatar.src = newDialog.author.avatar;
+    offerDialog.replaceChild(templateCopy, lodgePanel);
+  };
 })();
---
+++ js/data.js
@@ -0,0 +1,228 @@
+'use strict';
+window.offersList = [
+  {
+    'author': {
+      'avatar': 'img/avatars/user01.png'
+    },
+
+    'offer': {
+      'title': 'Большая уютная квартира',
+      'address': 'location.x, location.y',
+      'price': 1000,
+      'type': 'flat',
+      'rooms': 1,
+      'guests': 22,
+      'checkin': '12:00',
+      'checkout': '13:00',
+      'features': [
+        'wifi',
+        'dishwasher',
+        'parking'],
+      'description': '',
+      'photos': ''
+    },
+
+    'location': {
+      'x': 250,
+      'y': 200
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user02.png'
+    },
+
+    'offer': {
+      'title': 'Маленькая неуютная квартира',
+      'address': 'location.x, location.y',
+      'price': 5000,
+      'type': 'flat',
+      'rooms': 3,
+      'guests': 2,
+      'checkin': '14:00',
+      'checkout': '12:00',
+      'features': [
+        'wifi',
+        'dishwasher',
+        'parking',
+        'elevator'],
+      'description': '',
+      'photos': ''
+    },
+
+    'location': {
+      'x': 350,
+      'y': 150
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user03.png'
+    },
+
+    'offer': {
+      'title': 'Огромный прекрасный дворец',
+      'address': 'location.x, location.y',
+      'price': 3000,
+      'type': 'bungalo',
+      'rooms': 4,
+      'guests': 3,
+      'checkin': '12:00',
+      'checkout': '12:00',
+      'features': [
+        'wifi',
+        'conditioner',
+        'parking',
+        'elevator'],
+      'description': '',
+      'photos': ''
+    },
+
+    'location': {
+      'x': 450,
+      'y': 350
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user04.png'
+    },
+
+    'offer': {
+      'title': 'Маленький ужасный дворец',
+      'address': 'location.x, location.y',
+      'price': 5000,
+      'type': 'bungalo',
+      'rooms': 5,
+      'guests': 12,
+      'checkin': '12:00',
+      'checkout': '13:00',
+      'features': [
+        'wifi',
+        'conditioner',
+        'parking',
+        'elevator',
+        'washer'],
+      'description': '',
+      'photos': ''
+    },
+
+    'location': {
+      'x': 600,
+      'y': 300
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user05.png'
+    },
+
+    'offer': {
+      'title': 'Красивый гостевой домик',
+      'address': 'location.x, location.y',
+      'price': 7000,
+      'type': 'house',
+      'rooms': 3,
+      'guests': 8,
+      'checkin': '12:00',
+      'checkout': '13:00',
+      'features': [
+        'wifi',
+        'conditioner',
+        'parking',
+        'elevator',
+        'washer',
+        'dishwasher'],
+      'description': '',
+      'photos': ''
+    },
+
+    'location': {
+      'x': 820,
+      'y': 420
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user06.png'
+    },
+
+    'offer': {
+      'title': 'Некрасивый негостеприимный домик',
+      'address': 'location.x, location.y',
+      'price': 1500,
+      'type': 'house',
+      'rooms': 2,
+      'guests': 5,
+      'checkin': '13:00',
+      'checkout': '13:00',
+      'features': [
+        'wifi',
+        'conditioner',
+        'parking'],
+      'description': '',
+      'photos': ''
+    },
+
+    'location': {
+      'x': 340,
+      'y': 410
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user07.png'
+    },
+
+    'offer': {
+      'title': 'Уютное бунгало далеко от моря',
+      'address': 'location.x, location.y',
+      'price': 4500,
+      'type': 'bungalo',
+      'rooms': 3,
+      'guests': 3,
+      'checkin': '12:00',
+      'checkout': '13:00',
+      'features': [
+        'wifi',
+        'conditioner',
+        'parking',
+        'elevator'],
+      'description': '',
+      'photos': ''
+    },
+
+    'location': {
+      'x': 900,
+      'y': 455
+    }
+  },
+  {
+    'author': {
+      'avatar': 'img/avatars/user08.png'
+    },
+
+    'offer': {
+      'title': 'Неуютное бунгало по колено в воде',
+      'address': 'location.x, location.y',
+      'price': 8500,
+      'type': 'bungalo',
+      'rooms': 5,
+      'guests': 10,
+      'checkin': '14:00',
+      'checkout': '13:00',
+      'features': [
+        'wifi',
+        'conditioner',
+        'parking',
+        'elevator'],
+      'description': '',
+      'photos': ''
+    },
+
+    'location': {
+      'x': 835,
+      'y': 375
+    }
+  },
+];
