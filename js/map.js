b86c372a079379cdb3a83a94ef9d7c434ca1e3f8
dmitriykirilyuk <dimasishere@gmail.com>
2017-04-27 00:49:14 +0300

    повесил попап на ошибки

--- js/map.js
+++ js/map.js
@@ -1,20 +1,20 @@
 'use strict';
 (function () {
   var pinMap = document.querySelector('.tokyo__pin-map');
-  pinMap.addEventListener('click', window.activatePin);
-  pinMap.addEventListener('keydown', window.activatePinByKey);
-  var posLeftMax = 1200 - 37;
-  var postTopMax = 700 - 94;
+  var dragZoneWidth = 1200;
+  var dragZoneHeight = 700;
+  var posLeftMax = dragZoneWidth - 37;
+  var postTopMax = dragZoneHeight - 94;
   var postLeftMin = 0 - 37;
-  var drawPins = function (pins) {
+  window.drawPins = function (pins) {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < pins.length; i++) {
       var pin = window.getPinNode(pins[i], i);
       fragment.appendChild(pin);
     }
     pinMap.appendChild(fragment);
+    window.setPinEvents(pinMap, pins);
   };
-  drawPins(window.offersList);
 
   var draggablePin = document.querySelector('.pin__main');
   var addressField = document.querySelector('#address');
